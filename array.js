b2270a58dce8e8805b25f3e47793b64ebacf138c
Mits <hoanglonglpgxxx@gmail.com>
2024-03-05 16:52:43 +0700

    update array section

--- array.js
+++ array.js
@@ -22,4 +22,67 @@ console.log(obj1 === obj2);
 console.log(obj1 === obj3); //false vì khác ô nhớ?
 
 //context vs scope
-//context khác với scope, context là this0
+//context khác với scope, context là this
+
+//instantiation: khởi tạo class,  contrstor func chạy đầu tiên, khi extend class thì phải gọi constructor của nó - dùng super, khi đó this sẽ trỏ vào class đang gọi super(không phải class được super)
+/*
+ */
+/* class Player {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    introduce() {
+        console.log(`name is ${this.name}, type is ${this.type}`);
+    }
+}
+
+class Wiz extends Player {
+    constructor(name, type) {
+        super(name, type);
+    }
+    test() {
+        console.log(`this is ${this.name}`);
+    }
+}
+
+const wiz1 = new Wiz('test1', 'test2');
+console.log(wiz1.test()); */
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+    }
+}
+
+const newArr = new MyArray();
+newArr.push('hi');
+console.log(newArr);
