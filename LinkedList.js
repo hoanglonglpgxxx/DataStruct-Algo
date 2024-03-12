e61a289a51945489aacd1b0201fa1824d3aabad7
Mits <hoanglonglpgxxx@gmail.com>
2024-03-12 15:59:20 +0700

    done linked list

--- LinkedList.js
+++ LinkedList.js
@@ -10,7 +10,7 @@
  */
 
 //Mẫu of linked list
-const myLinkedList = {
+/* const myLinkedList = {
     head: {
         value: 10,
         next: {
@@ -96,6 +96,7 @@ class LinkedList {
 
     remove(index) {
         const leader = this.traverseToIndex(index - 1);
+        console.log('leader is', leader);
         const removeNode = leader.next;
         leader.next = removeNode.next;
         this.length--;
@@ -108,4 +109,124 @@ myLinkedList2.append(5);
 myLinkedList2.append(16);
 myLinkedList2.prepend(1);
 myLinkedList2.insert(2, 98);
+myLinkedList2.remove(1); */
+
+/**
+ * doubly linked list
+ *  traverse list backward, contain 1 additional pointer
+ */
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            prev: null
+        };
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value); // không cần gán lại giá trị newNode.prev vì ở class đã gán = null rồi
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(array);
+        return array;
+    }
+
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        };
+        const leader = this.traverseToIndex(index - 1);
+        console.log(leader);
+        const follower = leader.next;
+        leader.next = newNode;
+        newNode.next = follower;
+        newNode.prev = leader;
+        follower.prev = newNode;
+        this.length++;
+        return this.printList();
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
+    remove(index) {
+        const leader = this.traverseToIndex(index - 1);
+        const removeNode = leader.next;
+        leader.next = removeNode.next;
+        leader.prev = removeNode.prev;
+        this.length--;
+        return this.printList();
+    }
+
+    //REVERSE A LINKED LIST
+    reverse() {
+        if (!this.head.next) {
+            return this.head;
+        }
+        let first = this.head;
+        this.tail = this.head;
+        let second = first.next;
+        while (second) {
+            const temp = second.next;
+            second.next = first;
+            first = second;
+            second = temp;
+        }
+        this.head.next = null;
+        this.head = first;
+
+        return this.printList();
+    }
+}
+
+const myLinkedList2 = new DoublyLinkedList(10);
+myLinkedList2.append(5);
+myLinkedList2.append(16);
+myLinkedList2.prepend(1);
+myLinkedList2.insert(2, 98);
 myLinkedList2.remove(1);
+myLinkedList2.reverse();
--- note.md
+++ note.md
@@ -20,6 +20,36 @@
 - Hash collision: xung đột băm
 - idempotent
 
+- Linked list
+
+* fast insert, fast delete, ordered, flexible size
+* slow lookup, more memory
+
+- Single vs doubly linked list
+
+* single: less memory, less operation, little bit faster, but can traverse from front only (used widely)
+* doubly: can traverse both from back and front, if delete prev node-no need to traverse from head node and find what that prev node is (theorathically)
+
+- Stacks and Queues
+
+* limit operations can do on them
+* are linear data structure
+* allow to traverse (go to data element sequentially - tuần tự)
+* they are different only at how data is removed from structure
+  a. Stack - Last In First Out
+
+- lookup O(n)
+- pop O(1)
+- push O(1)
+- peek O(1): view that most top item
+
+b.Queues - First In First Out
+
+- lookup O(n)
+- enqueue O(1) - push
+- dequeue O(1) - remove first item in queue
+- peek O(1)
+
 //Khóa 28Tech BỎ: B1-B13, B12-B13(JS Advanced but bullshit vcl???)
-/xem lại vid 10-13
-watching vid 14
+/xem lại vid DSnA 10-13,20, read more abt linked list
+watching vid 5 folder 9
